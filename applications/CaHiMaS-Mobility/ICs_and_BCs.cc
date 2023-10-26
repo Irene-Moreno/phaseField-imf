a1dfd862219c621771e70b234c3983adf4599af5
Irene-Moreno <irene.moreno.2020@uni.strath.ac.uk>
2023-10-26 16:37:46 +0100

    Introducing the CaHiMaS equations into the PRISMS framework. The non-mobility version considers Maxwell-Stefan as its diffusion model.

---
+++ applications/CaHiMaS-Mobility/ICs_and_BCs.cc
@@ -0,0 +1,56 @@
+// ===========================================================================
+// FUNCTION FOR INITIAL CONDITIONS
+// ===========================================================================
+
+#include <random>
+
+template <int dim, int degree>
+void customPDE<dim,degree>::setInitialCondition(const dealii::Point<dim> &p, const unsigned int index, double & scalar_IC, dealii::Vector<double> & vector_IC){
+    // ---------------------------------------------------------------------
+    // ENTER THE INITIAL CONDITIONS HERE
+    // ---------------------------------------------------------------------
+    // Enter the function describing conditions for the fields at point "p".
+    // Use "if" statements to set the initial condition for each variable
+    // according to its variable index
+
+    // The initial condition is a nearly homogeneous system with some gaussian
+    // noise in the composition.
+
+    // Define random generator with Gaussian distribution
+    static std::default_random_engine generator(std::random_device{}());
+    static std::normal_distribution<double> dist(0.5, 0.25);
+
+    if (index == 0){
+        scalar_IC = 0.5 + dist(generator);
+        scalar_IC = std::max(0.0, std::min(1.0, scalar_IC));
+    }
+    else {
+        scalar_IC = 0.0;
+    }
+
+    // ---------------------------------------------------------------------
+}
+
+// ===========================================================================
+// FUNCTION FOR NON-UNIFORM DIRICHLET BOUNDARY CONDITIONS
+// ===========================================================================
+
+template <int dim, int degree>
+void customPDE<dim,degree>::setNonUniformDirichletBCs(const dealii::Point<dim> &p, const unsigned int index, const unsigned int direction, const double time, double & scalar_BC, dealii::Vector<double> & vector_BC)
+{
+    // --------------------------------------------------------------------------
+    // ENTER THE NON-UNIFORM DIRICHLET BOUNDARY CONDITIONS HERE
+    // --------------------------------------------------------------------------
+    // Enter the function describing conditions for the fields at point "p".
+    // Use "if" statements to set the boundary condition for each variable
+    // according to its variable index. This function can be left blank if there
+    // are no non-uniform Dirichlet boundary conditions. For BCs that change in
+    // time, you can access the current time through the variable "time". The
+    // boundary index can be accessed via the variable "direction", which starts
+    // at zero and uses the same order as the BC specification in parameters.in
+    // (i.e. left = 0, right = 1, bottom = 2, top = 3, front = 4, back = 5).
+
+
+    // -------------------------------------------------------------------------
+
+}
