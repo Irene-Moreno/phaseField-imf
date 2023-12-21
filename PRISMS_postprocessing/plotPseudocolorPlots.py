75e73465d4bd4f335339dc8c04cce3220406eb99
Irene-Moreno <irene.moreno.2020@uni.strath.ac.uk>
2023-12-21 18:03:51 +0000

    Edit to remove all white padding, axes and colorbar from the binary img

--- PRISMS_postprocessing/plotPseudocolorPlots.py
+++ PRISMS_postprocessing/plotPseudocolorPlots.py
@@ -3,11 +3,14 @@
 # READ THE VTU SOLUTION FILES AND PLOT THE 2D COLORMAPS
 ##################################################################################
 
+import io
+import cv2
 import meshio
 import time
 import numpy as np
 from os import listdir
 from os.path import isfile, join
+import matplotlib as mpl
 from matplotlib import pyplot as plt
 import matplotlib.tri as tri
 import colormaps as cmaps
@@ -15,17 +18,19 @@ from calculateInductionTime import get_sorted_list_of_files
 from calculatePhaseFraction import read_VTU_for_coords_and_data
 
 start_time = time.time()
+mpl.rcParams['savefig.pad_inches'] = 0
 
-def plotPseudocolorPlot(ordered_filedirs_f, filesdir_f, binary):
+def plotPseudocolorPlot(ordered_filedirs_f, filesdir_f, binary, savepscplots):
 
     if binary == True:
         cmap = 'binary_r'
     else:
         cmap = cmaps.berlin
 
-    raw_img_f = []
+    raw_img_f = []; times_f = []
     for fdir in ordered_filedirs_f:
         x_f, y_f, v_f, block_f, time_f = read_VTU_for_coords_and_data(filesdir_f + '/' + fdir)
+        times_f.append(time_f)
 
         triang = tri.Triangulation(x_f, y_f)
         fig1, ax1 = plt.subplots()
@@ -33,17 +38,25 @@ def plotPseudocolorPlot(ordered_filedirs_f, filesdir_f, binary):
         tpc = ax1.tripcolor(triang, v_f, shading='gouraud', cmap=cmap, vmin=0, vmax=1)
         ax1.set_ylim(0, 800)
         ax1.set_xlim(0, 800)
-        fig1.colorbar(tpc)
-        fig1.savefig(filesdir_f + '/' + f"snapshot-{time_f}.png", dpi=300)
+        cbar = fig1.colorbar(tpc)
+        # NOTE: If axis labels are added, they will need to be removed BEFORE saving to a buffer
+        # right now there is no issue since they are inside the if
 
-        fig1.canvas.draw()
-        temp_canvas = fig1.canvas
-        s, (width, height) = temp_canvas.print_to_buffer()
-        raw_img_i = np.frombuffer(s, np.uint8).reshape((height, width, 4))  # Will round down floats but
-        raw_img_i = raw_img_i[-1]
-        raw_img_f.append(raw_img_i)                                                          # needed for the contour plot
-        plt.close(fig1)                                                                      # (our snapshots are ints anyway)
-    return raw_img_f
+        if savepscplots == True and binary == False:
+            ax1.set_ylabel('Y axis')
+            ax1.set_xlabel('X axis')
+            fig1.savefig(filesdir_f + '/' + f"snapshot-{time_f}.png", bbox_inches='tight', transparent="True", pad_inches=0, dpi=500)
+        elif binary == True:
+            ax1.set_axis_off()
+            cbar.remove()
+            buffer = io.BytesIO()  # Save the image directly into a buffer
+            fig1.savefig(buffer, bbox_inches='tight', transparent="True", pad_inches=0, format='png')
+            buffer.seek(0)
+            raw_img_i = cv2.imdecode(np.frombuffer(buffer.read(), dtype=np.uint8), cv2.IMREAD_GRAYSCALE)
+
+        raw_img_f.append(raw_img_i)
+        plt.close()
+    return raw_img_f, times_f
 
 
 if __name__ == '__main__':
@@ -52,6 +65,6 @@ if __name__ == '__main__':
     files = [f for f in listdir(filesdir) if isfile(join(filesdir, f)) and f.endswith('.vtu')]
 
     ordered_filedirs = get_sorted_list_of_files(files)
-    raw_imgs = plotPseudocolorPlot(ordered_filedirs, filesdir, binary=False)
+    raw_imgs = plotPseudocolorPlot(ordered_filedirs, filesdir, binary=False, savepscplots=True)
 
     print("--- %s seconds ---" % (time.time() - start_time))
